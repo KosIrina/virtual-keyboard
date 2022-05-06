bc2dedc2503180607b84eb756d9ed657d6a2b1bb
Irina Kosyanchuk <kosyanchuk.ira@gmail.com>
2022-05-07 02:13:38 +0300

    feat: add creation of main body parts and styles to them

---
+++ src/create-body.js
@@ -0,0 +1,28 @@
+export default function createBody() {
+  const body = document.querySelector('body');
+  const container = document.createElement('div');
+  container.classList.add('container');
+  body.append(container);
+
+  const title = document.createElement('h1');
+  title.classList.add('title');
+  title.innerText = 'Virtual Keyboard';
+  container.append(title);
+
+  const description = document.createElement('p');
+  description.classList.add('description');
+  description.innerHTML = 'This keyboard was created in the Windows operating system<br>To change the language, use the following keyboard shortcut: <i>left Alt + left Shift</i>';
+  container.append(description);
+
+  const textArea = document.createElement('textarea');
+  textArea.classList.add('textarea');
+  container.append(textArea);
+
+  const keyboardBackground = document.createElement('div');
+  keyboardBackground.classList.add('keyboard-background');
+  container.append(keyboardBackground);
+
+  const keyboard = document.createElement('div');
+  keyboard.classList.add('keyboard');
+  keyboardBackground.append(keyboard);
+}
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './style.css';
+import './style.scss';
+import createBody from './create-body';
+
+createBody();
