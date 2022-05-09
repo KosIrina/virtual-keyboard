b9a561bdf7d94bbdf32be093441e7edc3ca376c0
Irina Kosyanchuk <kosyanchuk.ira@gmail.com>
2022-05-09 06:36:41 +0300

    fix: adjust keypresseshighlights

--- src/highlight-pressed-key.js
+++ src/highlight-pressed-key.js
@@ -17,8 +17,10 @@ export function highlightKeyByMouse(event) {
 }
 
 export function removeKeyHighlight(event) {
-  if (event.code.length > 0 && document.querySelector(`.key[data-key-code=${event.code}]`) !== null) {
-    document.querySelector(`.key[data-key-code=${event.code}]`).classList.remove('active');
+  if (event.code) {
+    if (event.code.length > 0 && document.querySelector(`.key[data-key-code=${event.code}]`) !== null) {
+      document.querySelector(`.key[data-key-code=${event.code}]`).classList.remove('active');
+    }
   } else if (event.target.dataset.keyCode) {
     document.querySelector(`.key[data-key-code=${event.target.dataset.keyCode}]`).classList.remove('active');
   }
